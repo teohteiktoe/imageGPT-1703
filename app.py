b508e65aee56c751a5f66b43d87d6115fbe5253b
Teoh TeikToe <ttt@mensa.org.sg>
2023-03-17 14:33:03 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+from flask import Flask,request,render_template
+import replicate
+import os
+
+os.environ["REPLICATE_API_TOKEN"] = "787f515cb0624813736c11e7fefec66473394f02"
+model = replicate.models.get("tstramer/midjourney-diffusion")
+version = model.versions.get("436b051ebd8f68d23e83d22de5e198e0995357afef113768c20f0b6fcef23c8b")
+
+app = Flask(__name__)
+
+
+@app.route("/",methods=["GET","POST"])
+def index():
+    if request.method == "POST":
+        p = request.form.get("txt")
+        inputs = {"prompt":p}
+        out = version.predict(**inputs)
+        return(render_template("index.html",result=out[0]))
+    else:
+        return(render_template("index.html",result="waiting"))
+    
+if __name__ == "__main__":
+    app.run()
+
+
+# In[ ]:
+
+
+
+
